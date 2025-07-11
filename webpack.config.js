ef2e3b5f6e407f4ea24afc712aee800a635a98a2
Abisha <abirubyjane@gmail.com>
2025-07-11 12:39:49 +0300

    Second commit-corrected

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,34 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // точка входа
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true, // очищать dist перед сборкой
+    clean: true, 
+    
+  },
+  devServer: {
+    static: './dist',  // или другая папка с вашими файлами
+    hot: true,
+    open: true,
+    port: 8080
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'], // обрабатываем css
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.css', // имя итогового css
+      filename: 'style.css',
     }),
     new HtmlWebpackPlugin({
-      template: './src/index.html', // шаблон html
+      template: './src/index.html',
     }),
   ],
-  mode: 'development', // или 'production' для оптимизации
+  mode: 'development', 
 };
